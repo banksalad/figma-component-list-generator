86ed941940cf1ab9e80a971a70e5dd5886984818
JoonHo Park <cloud@rainist.com>
2021-02-12 01:00:34 +0900

    BPL-3665 Apply Lint Warnings & Errors

--- code.ts
+++ code.ts
@@ -1,4 +1,4 @@
-console.log(`Extract Components in ${figma.currentPage.name}`)
+console.log(`Extract Components in ${figma.currentPage.name}`);
 
 // Each ComponentSet has variant components as its children
 const componentSetNodeList: ComponentSetNode[] = [];
@@ -27,31 +27,33 @@ const trackedComponentList: (ComponentNode | ComponentSetNode)[] = [];
 console.log(`ComponentSetNode(${componentSetNodeList.length})`);
 componentSetNodeList.forEach(componentSetNode => {
   // utilize this value later if needed
-  let validComponentNumber = componentSetNode.name.trim().match(validComponentPrefixRegex)?.[1];
+  const trimmedName = componentSetNode.name.trim();
+  const validComponentNumber = trimmedName.match(validComponentPrefixRegex)?.[1];
   if (validComponentNumber != null) {
-    console.log(`- [tracked] ${componentSetNode.name} (${componentSetNode.children.length} variants)`);
+    console.log(`- [tracked] ${trimmedName} (${componentSetNode.children.length} variants)`);
     trackedComponentList.push(componentSetNode);
   } else {
-    console.log(`- [invalid] ${componentSetNode.name} (${componentSetNode.children.length} variants) does not match ${validComponentPrefixRegex}`);
+    console.log(`- [invalid] ${trimmedName} does not match ${validComponentPrefixRegex}`);
   }
   componentSetNode.children.forEach(componentNode => {
-    console.log(`- [variant] ${componentNode.name}`);
-  })
+    console.log(`- [variant] ${componentNode.name.trim()}`);
+  });
 });
 
 console.log(`ComponentNode(${componentNodeList.length})`);
 componentNodeList.forEach(componentNode => {
-  let parent = componentNode.parent;
+  const trimmedName = componentNode.name.trim();
+  const parent = componentNode.parent;
   if (parent != null && parent.type === 'COMPONENT_SET') { // exclude variant component
-    console.log(`- [ignored] ${componentNode.name} is already taken care of in component set ${parent.name}`);
+    console.log(`- [ignored] ${trimmedName} is already added by component set ${parent.name}`);
   } else {
     // utilize this value later if needed
-    let validComponentNumber = componentNode.name.trim().match(validComponentPrefixRegex)?.[1];
+    const validComponentNumber = trimmedName.match(validComponentPrefixRegex)?.[1];
     if (validComponentNumber != null) {
-      console.log(`- [tracked] ${componentNode.name}`);
+      console.log(`- [tracked] ${trimmedName}`);
       trackedComponentList.push(componentNode);
     } else {
-      console.log(`- [invalid] ${componentNode.name} does not match ${validComponentPrefixRegex}`);
+      console.log(`- [invalid] ${trimmedName} does not match ${validComponentPrefixRegex}`);
     }
   }
 });
